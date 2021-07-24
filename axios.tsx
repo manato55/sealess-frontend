7cc4dab4c8edcc2b4f3857f97552ab3822f914db
Manato Kawamura <ma-kawamura@kentem.co.jp>
2021-07-24 17:28:59 +0900

    const追加

--- axios.tsx
+++ axios.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios'; 
 
 const instance = axios.create({
-    baseURL : 'http://127.0.0.1:82/api'
+    baseURL : process.env.NEXT_PUBLIC_API_ENDOPOINT
 }); 
 
 instance.interceptors.request.use(
     (config) => {
-        axios.defaults.baseURL = 'http://127.0.0.1:82/api/'
+        axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_ENDOPOINT
         config.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
         return config;
     }
